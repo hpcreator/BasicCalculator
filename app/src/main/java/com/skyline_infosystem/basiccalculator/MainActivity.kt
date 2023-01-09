fadb6bdf080961f76828d53e914f47f4c867e017
harsh_patel <harsh.patel@opnexcellinc.com>
2023-01-09 09:12:44 +0530

    Initial commit

---
+++ app/src/main/java/com/skyline_infosystem/basiccalculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.skyline_infosystem.basiccalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
